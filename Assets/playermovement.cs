82a24f1a4b4a92f4daf8d6c8de5108e020b550c7
Creanbl <geimerwolf@gmail.com>
2026-03-16 14:13:05 +0200

    fix playermovement.cs

--- Assets/playermovement.cs
+++ Assets/playermovement.cs
@@ -16,18 +16,12 @@ public class playermovement : MonoBehaviour
     void Update()
     {
         float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Horizontal");
-
-if(Mathf.Abs(xInput) > 0)
-        {
-            body.velocity = new Vector2(xInput*speed, body.velocity.y);
-        }
-if(Mathf.Abs(yInput) > 0)
-        {
-            body.velocity = new Vector2(yInput*speed, body.velocity.x);
+        
+        if(Mathf.Abs(xInput) != 0f) {
+            body.velocity += new Vector2(xInput*speed, 0);
         }
 
-        Vector2 direction = new Vector2(xInput, yInput).normalized;
+        Vector2 direction = new Vector2(xInput, 0).normalized;
         body.velocity = direction * speed;
     }
 }
