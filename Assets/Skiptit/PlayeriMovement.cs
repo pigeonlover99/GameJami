81a6f879c96897b3f06984db272c0309dd6d93e7
Creanbl <geimerwolf@gmail.com>
2026-03-19 14:27:32 +0200

    Focus, M

--- Assets/Skiptit/PlayeriMovement.cs
+++ Assets/Skiptit/PlayeriMovement.cs
@@ -1,52 +1,112 @@
+using System;
+using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 16f;
+    private float jumpingPower = 13f;
     private bool isFacingRight = true;
 
+    public float IFrames;
+    [SerializeField] float MaxIframes;
+    [SerializeField] private Vector2 size;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+
     [SerializeField] private Animator animator;
- 
-    void Update()
+    [SerializeField] private Slider fireBar;
+    [SerializeField] private Slider iceBar;
+
+    public int FireHealth;
+    public int IceHealth;
+    public int MaxHealth;
+    public int CoyoteTime;
+
+    private void Start()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            Debug.Log("toimii");
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        }
-
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
-
-        Flip();
+        FireHealth = MaxHealth;
+        IceHealth = MaxHealth;
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-        if (speed > 0)
-        {
-            animator.SetBool("isWalking",true);
-        }
+        horizontal = Input.GetAxisRaw("Horizontal");
+        animator.SetBool("isWalking", horizontal != 0f);
+
+        if (horizontal == 0f) rb.velocity = new Vector2(0f, rb.velocity.y);
         else
         {
-            animator.SetBool("isWalking",false);
-        
+            if (horizontal > 0f) rb.velocity = new Vector2(speed, rb.velocity.y);
+            else rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+
+        rb.gravityScale = 3f;
+        if (IFrames < (MaxIframes * 0.66))
+        {
+            if (Input.GetButton("Jump"))
+            {
+                if (CoyoteTime > 0) {
+                    rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+                }
+                else if (rb.velocity.y > 0f)
+                {
+                    rb.gravityScale = 2.5f;
+                }
+                CoyoteTime = 0;
+            }
+            else if (!IsGrounded()) rb.gravityScale = 5f;
+            
+            Flip();
+            //rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+            rb.AddForce(new Vector2(horizontal * speed, 0f));
+        }
+
+        if (IFrames > 0f) IFrames -= 1f;
+        if (IceHealth <= 0 || FireHealth <= 0) Destroy(gameObject);
+
+        if (fireBar != null) {
+            fireBar.value = MaxHealth - FireHealth;
+            fireBar.maxValue = MaxHealth;
+        }
+        if (iceBar != null) {
+            iceBar.value = MaxHealth - IceHealth;
+            iceBar.maxValue = MaxHealth;
         }
     }
 
+     private void OnTriggerStay2D(Collider2D collision)
+     {
+        if ((collision.tag == "HostileFire" || collision.tag == "HostileIce") && IFrames <= 0f)
+        {
+            Debug.Log("!!!!!! HIT !!!!!!");
+
+            bool facingLeft = transform.position.x > collision.transform.position.x;
+            Vector2 vel = facingLeft ? new Vector2(6f, 6f) : new Vector2(-6f, 6f);
+            rb.AddForce(vel, ForceMode2D.Impulse);
+
+            if (collision.tag == "HostileFire") FireHealth--;
+            else IceHealth--;
+
+            IFrames = MaxIframes;
+        }
+     }
+
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.8f, groundLayer);
+        if (Physics2D.BoxCast(transform.position, size, 0, -transform.up, 0.4f, groundLayer))
+        {
+            if (CoyoteTime < 5) CoyoteTime++;
+            return true;
+        }
+        else {
+            if (CoyoteTime > 0) CoyoteTime--;
+            return false;
+        }
     }
 
     private void Flip()
